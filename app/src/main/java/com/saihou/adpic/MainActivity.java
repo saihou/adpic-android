e0d8515a9216ebf0d92e7d93e4dc0f696004f780
saihou <sai-hou@hotmail.com>
2016-02-19 22:28:02 -0800

    Added icons

--- app/src/main/java/com/saihou/adpic/MainActivity.java
+++ app/src/main/java/com/saihou/adpic/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.app_name);
 
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
