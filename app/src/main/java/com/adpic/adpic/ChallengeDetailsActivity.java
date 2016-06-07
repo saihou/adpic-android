4aa0db3ac7aaaa658f8c871fe0f4f3256da1f351
sai <sai.chue@playphone.com>
2016-06-06 17:16:38 -0700

    Changed challenge details page

--- app/src/main/java/com/adpic/adpic/ChallengeDetailsActivity.java
+++ app/src/main/java/com/adpic/adpic/ChallengeDetailsActivity.java
@@ -3,8 +3,11 @@ package com.adpic.adpic;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,8 +19,15 @@ public class ChallengeDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge_details);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         if (Utils.mostRecentChallengeClicked != null) {
             ChallengeCardData data = Utils.lookupChallenge();
+
+            getSupportActionBar().setTitle(data.getMerchantName());
+            ImageView actionBarBackground = (ImageView) findViewById(R.id.action_bar_image);
 
             CardView cv = (CardView) findViewById(R.id.challenge_card);
             TextView merchantName = (TextView) findViewById(R.id.merchant_name);
@@ -48,11 +58,23 @@ public class ChallengeDetailsActivity extends AppCompatActivity {
                 }
                 Bitmap scaledBitmap = Utils.getCroppedBitmapRoundRect(getApplicationContext(), Bitmap.createScaledBitmap(bitmap, width, height, false));
                 picture.setImageBitmap(scaledBitmap);
+                actionBarBackground.setImageBitmap(scaledBitmap);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         } else {
             // Get lost, what are you doing here??
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
